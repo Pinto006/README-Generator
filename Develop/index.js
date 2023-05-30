585e22f09dfa0fbbb987b63ccecd718cdc55e7ae
Wave FC <malloriepinto89@gmail.com>
2023-05-29 18:03:29 -0700

    adding prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+// const fs = require('fs');
 
+// const generateREADME = 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?',
+    }, 
+    {
+        type: 'input',
+        name: 'descripton',
+        message: 'What is the project description?',
+    }, 
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the instalation instructions?',
+    }, 
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the project usage?',
+    }, 
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Who contributed to this project?',
+    }, 
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your contact email for questions?',
+    }, 
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'License?',
+        choices: ['MIT', 'ISC', 'GNUPLv3'],
+        filter(value) {
+            return value.toLowerCase();
+        }
+    }, 
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile() {  //fleName, data
+    return inquirer.prompt(questions)
+    .then((answers)=>{
+        console.log(answers)
+        return answers
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
+writeToFile();
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
